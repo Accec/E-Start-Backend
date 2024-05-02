656323eef3ffa69f2b6007886f54f061dae4c006
xiaoqi <qi@salusec.io>
2024-05-02 22:51:31 +0800

    init: config

---
+++ src/config/__init__.py
@@ -0,0 +1,31 @@
+import os
+import logging
+import importlib
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    _instance = None
+
+    def __new__(cls) :
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.load_config()
+        return cls._instance
+    
+    def __getattr__(self, name):
+        return getattr(self.config, name)
+
+    def load_config(self):
+        """
+        加载配置
+        """
+        mode = os.environ.get('MODE', 'SAMPLE').lower()
+        try:
+            config_module = importlib.import_module(f'.{mode}', package=__package__)
+            self.config = config_module
+            logging.info(f"[Config] - [{mode}] is loaded")
+        except ImportError:
+            logging.error(f"[Config] - [{mode}] cannot be imported, falling back to [SAMPLE]")
+            from . import sample
+            self.config = sample
