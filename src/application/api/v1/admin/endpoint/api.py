82f8b5d1d727d4d1728c63b4ba84fe5e4882fdb9
Accec <jizai0803@gmail.com>
2024-05-24 20:10:23 +0800

    init:

---
+++ src/application/api/v1/admin/endpoint/api.py
@@ -0,0 +1,30 @@
+from utils.router import AdminBlueprint
+from utils.util import http_response
+from utils.response import Successfully, ArgsInvalidError, RateLimitError
+
+from sanic.request import Request
+from sanic_ext import validate, openapi
+
+from . import serializers
+from models import User, Log, Role
+
+from modules.rate_limit import rate_limit
+from modules.encryptor import hash_password
+from modules.auth import jwt
+
+JwtAuth = jwt.JwtAuth()
+
+@AdminBlueprint.post("/endpoints")
+@openapi.summary("endpoints")
+@openapi.description("endpoints")
+@openapi.secured("token")
+@openapi.response(status=200, content={"application/json": serializers.SuccessfullyResponse.model_json_schema()}, description="Successfully")
+@openapi.response(status=400, content={"application/json": serializers.ArgsInvalidResponse.model_json_schema()}, description="Args invalid")
+@openapi.response(status=401, content={"application/json": serializers.RequestErrorResponse.model_json_schema()}, description="Token expied")
+@openapi.response(status=403, content={"application/json": serializers.AuthorizedErrorResponse.model_json_schema()}, description="Authorized error")
+@openapi.response(status=500, content={"application/json": serializers.RequestErrorResponse.model_json_schema()}, description="Request error")
+@openapi.response(status=429, content={"application/json": serializers.RateLimitResponse.model_json_schema()}, description="Rate limit")
+@JwtAuth.permissions_authorized()
+async def admin_get_endpoints(request: Request):
+    
+    return http_response(status = 200, code = Successfully.code, msg = Successfully.msg)
